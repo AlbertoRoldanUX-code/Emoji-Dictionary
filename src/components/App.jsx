a4dbf6588b6b08d6259789853fdadb87af66b8f6
Alberto Roldán <a-f-r@hotmail.es>
2021-02-26 16:52:04 +0100

    Update App.jsx with 2 more Entry components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -16,38 +16,20 @@ function App(){
           />
 
           <Entry 
-            
-
+            emoji="🙏"
+            name="Person With Folded Hands"
+            definition="Two hands pressed together. Is currently very introverted, saying a
+              prayer, or hoping for enlightenment. Is also used as a “high five”
+              or to say thank you."
           />
 
-
-        
-          <div className="term">
-            <dt>
-              <span className="emoji" role="img" aria-label="Tense Biceps">
-                🙏
-              </span>
-              <span>Person With Folded Hands</span>
-            </dt>
-            <dd>
-              Two hands pressed together. Is currently very introverted, saying a
-              prayer, or hoping for enlightenment. Is also used as a “high five”
-              or to say thank you.
-            </dd>
-          </div>
-          <div className="term">
-            <dt>
-              <span className="emoji" role="img" aria-label="Tense Biceps">
-                🤣
-              </span>
-              <span>Rolling On The Floor, Laughing</span>
-            </dt>
-            <dd>
-              This is funny! A smiley face, rolling on the floor, laughing. The
+          <Entry 
+            emoji="🤣"
+            name="Rolling On The Floor, Laughing"
+            definition="This is funny! A smiley face, rolling on the floor, laughing. The
               face is laughing boundlessly. The emoji version of “rofl“. Stands
-              for „rolling on the floor, laughing“.
-            </dd>
-          </div>
+              for „rolling on the floor, laughing“."
+          />
         </dl>
       </div>);
 }
